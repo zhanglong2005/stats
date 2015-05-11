b21b0f7f148344e0fc0355538de2ce7bd2129d63
Benjamin Borbe <bborbe@rocketnews.de>
2015-05-11 17:32:51 +0200

    test service

--- entry/service/stats_entry_service_test.go
+++ entry/service/stats_entry_service_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	. "github.com/bborbe/assert"
+	stats_entry "github.com/bborbe/stats/entry"
+	stats_entry_storage "github.com/bborbe/stats/entry/storage"
 )
 
 func TestNewHandlerImplementsService(t *testing.T) {
@@ -11,6 +13,66 @@ func TestNewHandlerImplementsService(t *testing.T) {
 	var i (*Service) = nil
 	err := AssertThat(r, Implements(i).Message("check implements server.Server"))
 	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCreate(t *testing.T) {
+	var err error
+	storage := stats_entry_storage.New("/tmp/stats_test.db", false)
+	service := New(storage)
+	if err = storage.Truncate(); err != nil {
+		t.Fatal(err)
+	}
+	entries, err := storage.FindEntrys()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = AssertThat(len(entries), Is(0)); err != nil {
+		t.Fatal(err)
+	}
+	entry, err := service.Create(&stats_entry.Entry{Value: 1337})
+	if err = AssertThat(err, NilValue()); err != nil {
+		t.Fatal(err)
+	}
+	if err = AssertThat(entry, NotNilValue()); err != nil {
+		t.Fatal(err)
+	}
+	if err = AssertThat(entry.Value, Is(1337)); err != nil {
+		t.Fatal(err)
+	}
+	entries, err = storage.FindEntrys()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = AssertThat(len(entries), Is(1)); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestList(t *testing.T) {
+	var err error
+	storage := stats_entry_storage.New("/tmp/stats_test.db", false)
+	service := New(storage)
+	if err = storage.Truncate(); err != nil {
+		t.Fatal(err)
+	}
+	entries, err := service.List()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = AssertThat(len(entries), Is(0)); err != nil {
+		t.Fatal(err)
+	}
+	err = storage.CreateEntry(&stats_entry.Entry{Value: 1337})
+	if err = AssertThat(err, NilValue()); err != nil {
+		t.Fatal(err)
+	}
+	entries, err = service.List()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = AssertThat(len(entries), Is(1)); err != nil {
 		t.Fatal(err)
 	}
 }
