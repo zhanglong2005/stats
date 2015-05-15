7777e292b955543a48537e6340e72d589f2c23ac
Benjamin Borbe <bborbe@rocketnews.de>
2015-05-16 00:52:12 +0200

    small improvments

--- files/js/controllers.js
+++ files/js/controllers.js
@@ -2,14 +2,15 @@
 
 var statsControllers = angular.module('statsControllers', []);
 
-var Nanosecond = 1
-var Microsecond          = 1000 * Nanosecond
-var Millisecond          = 1000 * Microsecond
-var Second               = 1000 * Millisecond
-var Minute               = 60 * Second
-var Hour                 = 60 * Minute
-
 statsControllers.controller('EntryCtrl', ['$scope', 'Entry', function ($scope, Entry) {
+
+  var Nanosecond = 1;
+  var Microsecond = 1000 * Nanosecond;
+  var Millisecond = 1000 * Microsecond;
+  var Second = 1000 * Millisecond;
+  var Minute = 60 * Second;
+  var Hour = 60 * Minute;
+
   $scope.stats = [];
   var statsFunc = function (result) {
     if (result) {
@@ -18,14 +19,17 @@ statsControllers.controller('EntryCtrl', ['$scope', 'Entry', function ($scope, E
       angular.forEach(result, function (entry) {
         if (last) {
           var valueDiff = entry.value - last.value;
-          var timeDiff = (entry.timestamp - last.timestamp) / Hour;
+          var timeDiff = entry.timestamp - last.timestamp;
           var stats = {
             timestamp: entry.timestamp,
             value: entry.value,
-            diff: valueDiff,
-            perhour: valueDiff / timeDiff,
+            valueDiff: valueDiff,
+            timeDiff: timeDiff,
+            diffPerHour: valueDiff / (timeDiff / Hour),
           };
-          this.push(stats);
+          if (valueDiff != 0) {
+            this.push(stats);
+          }
         }
         last = entry;
       }, stats);
@@ -35,9 +39,10 @@ statsControllers.controller('EntryCtrl', ['$scope', 'Entry', function ($scope, E
   $scope.entries = Entry.query({}, statsFunc);
   $scope.entry = {};
   $scope.submit = function () {
-    Entry.create($scope.entry);
-    $scope.entry = {};
-    $scope.entries = Entry.query({}, statsFunc);
+    Entry.create($scope.entry, function () {
+      $scope.entries = Entry.query({}, statsFunc);
+      $scope.entry = {};
+    });
   };
 }]);
 
--- files/js/filters.js
+++ files/js/filters.js
@@ -2,27 +2,66 @@
 
 var statsFilters = angular.module('statsFilters', []);
 
-statsFilters.filter('checkmark', function() {
-	return function(input) {
-		return input ? '\u2713' : '\u2718';
-	};
+statsFilters.filter('checkmark', function () {
+  return function (input) {
+    return input ? '\u2713' : '\u2718';
+  };
 });
 
-statsFilters.filter('timestampToDate', function() {
-	return function(input) {
-		var d = new Date(input/1000/1000);
-		return d.toISOString().slice(0,19).replace("T"," ");
-	};
+statsFilters.filter('timestampToDate', function () {
+  return function (input) {
+    var d = new Date(input / 1000 / 1000);
+    return d.toISOString().slice(0, 19).replace("T", " ");
+  };
 });
 
-statsFilters.filter('toFixed', function() {
-	return function(input,l) {
-		return input.toFixed(l);
-	};
+statsFilters.filter('timeToString', function () {
+  return function (input, l) {
+    var Nanosecond = 1
+    var Microsecond = 1000 * Nanosecond
+    var Millisecond = 1000 * Microsecond
+    var Second = 1000 * Millisecond
+    var Minute = 60 * Second
+    var Hour = 60 * Minute
+
+    var format = function(value) {
+      if (l) {
+        return value.toFixed(l);
+      }
+      return value;
+    };
+
+    if (input > Hour) {
+      return format(input / Hour) + ' hour';
+    }
+
+    if (input > Minute) {
+      return format(input / Minute) + ' min';
+    }
+
+    if (input > Second) {
+      return format(input / Second) + ' sec';
+    }
+
+    if (input > Millisecond) {
+      return format(input / Millisecond) + ' ms';
+    }
+
+    if (input > Microsecond) {
+      return format(input / Microsecond).toFixed(l) + ' µs';
+    }
+    return format(input) + 'ns';
+  };
 });
 
-statsFilters.filter('reverse', function() {
-	return function(items) {
-		return items.slice().reverse();
-	};
+statsFilters.filter('toFixed', function () {
+  return function (input, l) {
+    return input.toFixed(l);
+  };
+});
+
+statsFilters.filter('reverse', function () {
+  return function (items) {
+    return items.slice().reverse();
+  };
 });
